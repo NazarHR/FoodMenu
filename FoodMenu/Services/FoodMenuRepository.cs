461716f63287ac4dd348fb92b8ec7e09d2032a45
NazarHR <47197105+NazarHR@users.noreply.github.com>
2024-05-03 11:34:29 +0300

    Services
    added data access repository

---
+++ FoodMenu/Services/FoodMenuRepository.cs
@@ -0,0 +1,30 @@
+﻿using FoodMenu.Data;
+using FoodMenu.Data.Entitie;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodMenu.Services
+{
+    public class FoodMenuRepository
+    {
+        private readonly FoodMenuDBContext _dbContext;
+
+        public FoodMenuRepository(FoodMenuDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Tag>> GetTagsAsync()
+        {
+            return await _dbContext.Tags.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Dish>> GetDishesAsync()
+        {
+            return await _dbContext.Dishes.ToListAsync();
+        }
+        public async Task<IEnumerable<Dish>> GetDishesAsync(string fullOrPartitialName)
+        {
+            return await _dbContext.Dishes.Where(d=>d.Name.Contains(fullOrPartitialName)).ToListAsync();
+        }
+    }
+}
